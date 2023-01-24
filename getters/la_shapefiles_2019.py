0ecc1fef44d497576215334c573ed550884e9e96
j-gillam <107113232+j-gillam@users.noreply.github.com>
2023-01-24 17:39:49 +0000

    Update la_shapefiles_2019.py

--- getters/la_shapefiles_2019.py
+++ getters/la_shapefiles_2019.py
@@ -3,7 +3,7 @@ import altair as alt
 
 def get_la_shapefiles_2019() -> alt.Data:
 
-    geojson_la = "https://raw.githubusercontent.com/j-gillam/uk_geojson_topojson_datasets/main/la_clean_shapefiles_2019.geojson"
+    geojson_la = "https://raw.githubusercontent.com/j-gillam/geographical_iod_analysis/main/shapefiles/la_clean_shapefiles_2019.geojson"
     geodata_la = alt.Data(
         url=geojson_la, format=alt.DataFormat(property="features", type="json")
     )
